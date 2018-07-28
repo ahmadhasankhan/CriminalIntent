d2349da99a38012e63e5ab8205bd7f755958d12d
Ahmad Hassan <er.ahmad.hassan@gmail.com>
2018-07-28 13:20:25 +0530

    Added list using fragment

---
+++ app/src/main/java/com/example/ahmadhassan/criminalintent/Crime.java
@@ -0,0 +1,44 @@
+package com.example.ahmadhassan.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean sSolved;
+
+    public Crime(){
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean issSolved() {
+        return sSolved;
+    }
+
+    public void setsSolved(boolean sSolved) {
+        this.sSolved = sSolved;
+    }
+}
